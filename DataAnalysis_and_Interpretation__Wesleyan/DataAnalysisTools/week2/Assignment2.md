d6049c1c35683c6ad35a63b131c61a657821c940
Daisuke Hashimoto <hassy5407@gmail.com>
2016-04-24 01:53:46 -0700

    Assignment2

---
+++ DataAnalysis_and_Interpretation__Wesleyan/DataAnalysisTools/week2/Assignment2.md
@@ -0,0 +1,15 @@
+#syntax used to run an ANOVA
+[Link to Code Syntax](https://github.com/daisuke834/Coursera/blob/master/DataAnalysis_and_Interpretation__Wesleyan/DataAnalysisTools/week2/Assignment2_ChiSquare.py "Link to Code Syntax") <-Click Here
+
+#output
+[Link to Output](https://github.com/daisuke834/Coursera/blob/master/DataAnalysis_and_Interpretation__Wesleyan/DataAnalysisTools/week2/output.txt "Link to Output") <-Click Here
+
+# interpretation
+* Data: Adults age 20 to 50.
+* Explanatory Variable: CURRENT MARITAL STATUS which is collapsed into 6 ordered categories. 1. Married, 2. Living with someone as if married, 3. Widowed, 4. Divorced, 5. Separated, 6. Never Married
+* Response Variable: Alcohol abuse/dependence happen in the last 12 month
+
+Chi-Square test revealed that current marital status and alcohol abuse were significantly associated, X2=180.5, p=4.2e-37.
+
+Post hoc comparisons of alcohol abuse by pairs of marital statuses revealed that marital status and alcohol abuse were significantly associated for all of the comparison pairs (15 comparisons) because each p-value was smaller than Bonferroni-Adjustment=0.0017. The result also showed that those who have been never married tend to have the highest rate of alcohol abuse, and those who are married tend to have the lowest rate of alcohol abuse.
+![BarChart](barchart.png)
---
+++ DataAnalysis_and_Interpretation__Wesleyan/DataAnalysisTools/week2/Assignment2_ChiSquare.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+import pandas as pd  
+import numpy as np
+import scipy.stats
+import seaborn
+import matplotlib.pyplot as plt
+
+pd.set_option('display.width', 100)
+
+print('***********************************************')
+print('***********************************************')
+print('***********************************************')
+#_data = pd.read_csv('nesarc_pds.csv', low_memory=False)
+
+_data['MARITAL'] = pd.to_numeric(_data['MARITAL'], errors='coerce')
+_data['AGE'] = pd.to_numeric(_data['AGE'], errors='coerce')
+#Alcohol abuse/dependence happen in the last 12 month
+_data['S2BQ1B1'] = pd.to_numeric(_data['S2BQ1B1'], errors='coerce')
+
+#subset _data to adults age 20 to 50
+_sub1=_data[(_data['AGE']>=20) & (_data['AGE']<=50)]
+
+#make a copy of my new subsetted _data
+_sub2 = _sub1.copy()
+
+_recode1 = {1:'Married', 2:'Liv some', 3:'Widow', 4:'Divorced', 5:'Separa', 6:'Nev Marr'}
+_sub2.loc[:, 'MARITAL']= _sub2['MARITAL'].map(_recode1)
+
+_recode2 = {1:'Yes', 2: 'No'}
+_sub2.loc[:, 'ABUSE']= _sub2['S2BQ1B1'].map(_recode2)
+
+# contingency table of observed counts
+_ct1=pd.crosstab(_sub2['ABUSE'], _sub2['MARITAL'])
+print (_ct1)
+print('')
+
+# column percentages
+_colsum=_ct1.sum(axis=0)
+_colpct=_ct1/_colsum
+print(_colpct)
+print('')
+
+# chi-square
+print ('chi-square value, p value, expected counts')
+_cs1= scipy.stats.chi2_contingency(_ct1)
+print (_cs1)
+print('x2='+str(_cs1[0]))
+print('p='+str(_cs1[1]))
+print('')
+
+# set variable types 
+_sub3 = _sub2.copy()
+_sub3.loc[:, 'ABUSE']= _sub3['ABUSE'].map({'Yes':1, 'No':0})
+_sub3["MARITAL"] = _sub3["MARITAL"].astype('category')
+_sub3['ABUSE'] = pd.to_numeric(_sub3['ABUSE'], errors='coerce')
+
+# graph percent with nicotine dependence within each smoking frequency group 
+seaborn.factorplot(x="MARITAL", y="ABUSE", data=_sub3, kind="bar", ci=None)
+plt.xlabel('MARITAL')
+plt.ylabel('Alcohol abuse/dependence happen in last 12 month')
+
+print('***********************************************')
+_label = ['Married', 'Liv some', 'Widow', 'Divorced', 'Separa', 'Nev Marr']
+#_recode1 = {1:'Married', 2:'Liv some', 3:'Widow', 4:'Divorced', 5:'Separa', 6:'Nev Marr'}
+_p = np.ndarray((6,6), dtype=np.float)
+_x2 = np.ndarray((6,6), dtype=np.float)
+for _i in range(1,7):
+	for _j in range(1,_i):
+		_sub4 = _sub1.copy()
+		_recode3 = {_i: _recode1[_i], _j: _recode1[_j]}
+		_sub4.loc[:, 'MARITAL']= _sub4['MARITAL'].map(_recode3)
+		_sub4.loc[:, 'ABUSE']= _sub4['S2BQ1B1'].map(_recode2)
+		_ct2=pd.crosstab(_sub2['ABUSE'], _sub2['MARITAL'])
+		_cs2= scipy.stats.chi2_contingency(_ct2)
+		_p[_i-1,_j-1] = _cs2[1]
+		_p[_j-1,_i-1] = _cs2[1]
+		_x2[_i-1,_j-1] = _cs2[0]
+		_x2[_j-1,_i-1] = _cs2[0]
+for _i in range(1,7):
+	_p[_i-1,_i-1] = 1.0
+	_x2[_i-1,_i-1] = np.nan
+#_p = _p.round(2)
+#_x2 = _x2.round(2)
+
+print('Chi-Square')
+_x2_pd = pd.DataFrame(_x2, columns=_label, index=_label)
+print(_x2_pd)
+
+print('')
+print('P')
+_p_pd = pd.DataFrame(_p, columns=_label, index=_label)
+print(_p_pd)
+
+_m = 6*5
+_BonferroniAdjustment = 0.05 / _m
+print('')
+print('Number of Comparison='+str(_m))
+print('BonferroniAdjustment'+str(_BonferroniAdjustment))
+_tf = (_p<_BonferroniAdjustment)
+_tf_pd = pd.DataFrame(_tf, columns=_label, index=_label)
+print('')
+print('Statistically significant')
+print(_tf_pd)
